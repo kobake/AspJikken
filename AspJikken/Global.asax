109b045ab492a215f9b1c6f341ee3af7b7052a0a
kobake <kobake@users.sourceforge.net>
2016-06-05 11:32:39 +0900

    プロジェクト ファイルを追加します。

---
+++ AspJikken/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspJikken.MvcApplication" Language="C#" %>
---
+++ AspJikken/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspJikken.Startup))]
+namespace AspJikken
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
